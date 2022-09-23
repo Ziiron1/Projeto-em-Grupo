78881eec3856383326d1ecdfe6d274d1c131bdba
Ziiron1 <guilherme_profissional@yahoo.com.br>
2022-09-23 08:14:32 -0300

    Updatee

--- Page/Contact/contact.js
+++ Page/Contact/contact.js
@@ -1,5 +1,10 @@
-
-function onclick() {
-  document.write("Seu formulário foi enviado!!!")
-
+function valida() {
+  let valor_nome = document.forms['form-contact']['name'].value;
+  let valor_email = document.forms['form-contact']['e-mail'].value;
+  let valor_mensagem = document.forms['form-contact']['message'].value;
+  if (valor_nome == '' || valor_email == '' || valor_mensagem == '') {
+    alert('Preencha todos os campos, por favor.');
+  } else {
+    alert('Enviado com sucesso.');
+  }
 }
